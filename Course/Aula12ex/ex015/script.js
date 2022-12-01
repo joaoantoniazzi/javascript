d094a7b02661e10fecb4c0c08a63e7b488ddc4d5
João Antoniazzi <100880882+joaoantoniazzi@users.noreply.github.com>
2022-11-30 23:15:05 -0300

    Alteração ex015

--- Course/Aula12ex/ex015/script.js
+++ Course/Aula12ex/ex015/script.js
@@ -1,1 +1,11 @@
-//alert("Olá!")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('id#txtano')
+    var res = document.querySelector('id#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        window.alert('tudo ok')
+    }
+}
