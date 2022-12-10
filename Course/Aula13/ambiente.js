cdc6d3b20b0c38056fec1d7c15e5bb9721bd4292
João Antoniazzi <100880882+joaoantoniazzi@users.noreply.github.com>
2022-12-10 15:14:24 -0300

    Update ambiente.js

--- Course/Aula13/ambiente.js
+++ Course/Aula13/ambiente.js
@@ -1,6 +1,22 @@
+/*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
+*/
+
+/*
+var x = 1
+while (x <= 6) {
+    console.log(`Passo ${x}`)
+    x++
+}
+*/
+
+x = 1
+do {
+    console.log(`Passo ${x}`)
+    x++
+} while
