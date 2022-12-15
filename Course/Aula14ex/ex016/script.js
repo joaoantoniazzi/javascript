f82143b0b8486b756c36ec6f9a4ee2a97fd016c6
João Antoniazzi <100880882+joaoantoniazzi@users.noreply.github.com>
2022-12-15 00:07:49 -0300

    Alteração ex016

--- Course/Aula14ex/ex016/script.js
+++ Course/Aula14ex/ex016/script.js
@@ -1,16 +1,32 @@
-function iniciar() {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var res = document.getElementById('res')
-    var j = ''
+var inicio = document.getElementById('inicio')
+var fim = document.getElementById('fim')
+var passo = document.getElementById('passo')
+var res = document.getElementById('res')
+var j = ''
 
+function iniciar() {
     if (inicio.value.length==0 || fim.value.length==0 || passo.value.length==0) {
+        res.innerText = 'Impossível contar!'
         window.alert("[ERRO] Revise os dados e tente novamente!")
     } else {
-        for (i=Number(inicio.value); i<=Number(fim.value); i=i+Number(passo.value)) {
-            j = j+String(i)+'👉'
-        }
+        contar()
         res.innerText=j+'🏁'
+        j = ''
+    }
+}
+
+function contar() {
+    if (Number(passo.value)==0) {
+        window.alert('Passo impossível. Considerando PASSO = 1')
+        passo.value = 1
+    }
+    if (Number(passo.value)<0) {
+        for (i=Number(inicio.value); i>=Number(fim.value); i=i+Number(passo.value)) {
+            j = j+String(i)+' 👉 '
+        }
+    } else {
+        for (i=Number(inicio.value); i<=Number(fim.value); i=i+Number(passo.value)) {
+            j = j+String(i)+' 👉 '
+        }
     }
 }
